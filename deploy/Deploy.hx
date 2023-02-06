7e62bfa75cfc67453df2e4b7c0fb2ff36390c61c
jf <22012032+jobf@users.noreply.github.com>
2023-02-06 03:56:41 +0000

    demo deployment script

---
+++ deploy/Deploy.hx
@@ -0,0 +1,31 @@
+package deploy;
+
+import sys.io.File;
+
+using DateTools;
+
+class Deploy{
+	public static function main() {
+		
+		var host_remote = File.getContent("secrets/remote_host");
+		var path_remote = File.getContent("secrets/remote_path");
+
+		if(host_remote.length == 0 || path_remote.length == 0){
+			trace('Cannot deploy without secrets.');
+		}
+
+		var path_local = "export/html5/bin";
+
+		var path_version = Date.now().format("%d-%H-%M");
+		
+		var command:String = "scp";
+		
+		var args:Array<String> = [
+			"-r",
+			path_local,
+			'$host_remote:$path_remote/$path_version',
+		];
+
+		Sys.command(command, args);
+	}
+}
