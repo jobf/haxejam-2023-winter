f86f7b9f30b2afd75e7ef912f0e5fa47a170b0bd
jf <22012032+jobf@users.noreply.github.com>
2023-02-06 19:08:30 +0000

    show colelcted items on hud

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,9 +7,12 @@ import engine.building.Wall;
 import engine.map.BluePrint;
 import engine.tasks.laundry.Basket;
 import engine.tasks.laundry.Item;
+import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxGroup;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
@@ -21,7 +24,13 @@ class PlayState extends FlxState
 
 	var walls:FlxTypedGroup<Wall>;
 	var laundry:FlxTypedGroup<Item>;
+	var collected_laundry:FlxTypedGroup<Item>;
 	var basket:Basket;
+	// var main_camera:FlxCamera;
+	var hud_camera:FlxCamera;
+	var hud:Hud;
+
+
 
 
 	override public function create()
@@ -31,6 +40,9 @@ class PlayState extends FlxState
 		FlxG.worldBounds.width = 4096;
 		FlxG.worldBounds.height = 4096;
 
+		hud = new Hud();
+		add(hud);
+		
 		var floor_plan = BluePrint.generate_floor_plan();
 
 		edge_left = 40;
@@ -43,34 +55,52 @@ class PlayState extends FlxState
 		laundry = new FlxTypedGroup<Item>();
 		add(laundry);
 
+		collected_laundry = new FlxTypedGroup<Item>();
+		hud.add(collected_laundry);
+
 		var empty_spots:Array<Placement> = [];
-		
-		for (placement in Layout.placements(floor_plan, edge_left, edge_top, grid_size)) {
-			switch placement.location {
-				case WALL: place_wall(placement);
-				case BASKET: place_basket(placement);
-				case PLAYER: place_player(placement);
-				case EMPTY: empty_spots.push(placement);
+
+		for (placement in Layout.placements(floor_plan, edge_left, edge_top, grid_size))
+		{
+			switch placement.location
+			{
+				case WALL:
+					place_wall(placement);
+				case BASKET:
+					place_basket(placement);
+				case PLAYER:
+					place_player(placement);
+				case EMPTY:
+					empty_spots.push(placement);
 			}
 		}
-		
+
 		// shuffle the empty spots before distributing items
 		FlxG.random.shuffle(empty_spots);
-		
+
 		var index_empty_spot = 0;
-		
-		// distrbute laundry
+
+		// distribute laundry
 		var total_dirty_laundry = 8;
-		for (i in 0...total_dirty_laundry) {
+		for (i in 0...total_dirty_laundry)
+		{
 			place_dirty_laundry(empty_spots[i]);
 			index_empty_spot++;
 		}
+		collection_x = 10;
+		collection_y = FlxG.height - 42;
 
-
-		FlxG.camera.zoom = zoom_amount;
+		hud_camera = new FlxCamera(0,0,FlxG.camera.width, FlxG.camera.height);
+		hud_camera.zoom = 1;
+		hud_camera.alpha = 0.8;
+		hud_camera.bgColor = FlxColor.TRANSPARENT;
+		hud_camera.follow(hud.background, FlxCameraFollowStyle.NO_DEAD_ZONE);
+		
+		FlxG.cameras.add(hud_camera);
 	}
 
-	function place_player(spot:Placement) {
+	function place_player(spot:Placement)
+	{
 		player = new Actor({
 			x_start: spot.x_pixel,
 			y_start: spot.y_pixel,
@@ -85,7 +115,8 @@ class PlayState extends FlxState
 		controller = new Controller(player);
 	}
 
-	function place_wall(spot:Placement) {
+	function place_wall(spot:Placement)
+	{
 		walls.add(new Wall({
 			x: spot.x_pixel,
 			y: spot.y_pixel,
@@ -94,7 +125,8 @@ class PlayState extends FlxState
 		}));
 	}
 
-	function place_basket(spot:Placement) {
+	function place_basket(spot:Placement)
+	{
 		var basket_size = 64;
 		var basket_center = basket_size / 2;
 		basket = new Basket({
@@ -106,7 +138,8 @@ class PlayState extends FlxState
 		add(basket);
 	}
 
-	function place_dirty_laundry(spot:Placement) {
+	function place_dirty_laundry(spot:Placement)
+	{
 		laundry.add(new Item({
 			x: spot.x_pixel,
 			y: spot.y_pixel,
@@ -119,27 +152,85 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 		controller.update(FlxG.keys);
-		if(FlxG.keys.justPressed.I){
+		if (FlxG.keys.justPressed.I)
+		{
 			zoom(-1);
 		}
-		if(FlxG.keys.justPressed.O){
+		if (FlxG.keys.justPressed.O)
+		{
 			zoom(1);
 		}
-		if(FlxG.keys.justPressed.R){
+		if (FlxG.keys.justPressed.R)
+		{
 			FlxG.resetState();
 		}
+		// stop running through walls
 		FlxG.collide(player, walls);
+
+		// interact with items
+		FlxG.overlap(laundry, player, overlap_laundry_with_player);
+	}
+
+	function overlap_laundry_with_player(laundry:Item, player:Actor)
+	{
+		collect(laundry);
+	}
+
+	var collected_items:Array<Item> = [];
+	var collection_size:Int = 16;
+	var collection_size_gap:Int = 10;
+	var collection_x:Int;
+	var collection_y:Int;
+
+	function collect(laundry:Item)
+	{
+		laundry.kill();
+		var collected = new Item({
+			y: collection_y,
+			x: collection_x + ((collection_size + collection_size_gap) * collected_items.length),
+			size: collection_size,
+			color: laundry.config.color
+		});
+		collected.cameras  = [hud_camera];
+		collected_items.push(collected);
+		collected_laundry.add(collected);
+		collected.scrollFactor.x = 0;
+		collected.scrollFactor.y = 0;
 	}
 
 	var zoom_amount:Float = 0.5;
-	function zoom(direction:Int) {
+
+	function zoom(direction:Int)
+	{
 		zoom_amount += direction;
-		if(zoom_amount < 1){
+		if (zoom_amount < 1)
+		{
 			zoom_amount = 1;
 		}
-		else{
+		else
+		{
 			zoom_amount = 0.5;
 		}
 		FlxG.camera.zoom = zoom_amount;
+		hud_camera.zoom = 1;
+	}
+}
+
+
+class Hud extends FlxGroup{
+	public var background:FlxSprite;
+
+	public function new()
+	{
+		super();
+
+		// add a graphic for the hud_camera to follow
+		// it needs to be out of the bounds of the main camera
+		// otherwise we see the main camera content duplicated
+		
+		var x:Int = 100000;
+		background = new FlxSprite(x, 0);
+		background.makeGraphic(0, 0, FlxColor.TRANSPARENT);
+		add(background);
 	}
 }
